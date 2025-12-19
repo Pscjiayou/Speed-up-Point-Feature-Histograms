92ef94f2a58e6895320eded217d2e211d17bf5c0
Shicheng Peng <1789004387@qq.com>
2025-12-18 21:24:56 -0500

    final version with paper

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ PYTHON_BIN=${PYTHON_BIN:-python}
 echo ">>> Using python: $PYTHON_BIN"
 "$PYTHON_BIN" -m pip install --upgrade pip
 
-"$PYTHON_BIN" -m pip install numpy matplotlib open3d scipy
+"$PYTHON_BIN" -m pip install numpy matplotlib open3d scipy PyQt6
 
 echo ">>> Done. If you have multiple Python versions, you can run:"
 echo "    PYTHON_BIN=python3.10 ./install.sh"
